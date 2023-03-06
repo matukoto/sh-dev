e658ddf7be17eb2a9189fdb935cbbe3bd10ccd2a
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-03-06 23:05:31 +0900

    feat: ブランチ名を取得する処理

--- trial/branch.sh
+++ trial/branch.sh
@@ -4,8 +4,10 @@
 # ブランチ名の一部を変数 GITHUB_ENV に格納する
 GITHUB_HEAD_REF=release/TECHBLOG-22
 
-
 echo ${GITHUB_HEAD_REF}
 
 # これが欲しい物
-echo ${GITHUB_HEAD_REF#*/}
+echo ${GITHUB_HEAD_REF#*/} >> $GITHUB_ENV
+
+echo $GITHUB_ENV
+
