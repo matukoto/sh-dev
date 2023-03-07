ef3f0ba877a63c011d8739abe55e719c0fb661d8
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-03-07 23:07:53 +0900

    a

--- trial/yq.sh
+++ trial/yq.sh
@@ -6,3 +6,14 @@
 
 yq '.on.pull_request' wf.yaml
 
+2023-03-07
+なんか知らんけど､ GitHubActions で yq が使えない｡
+とてもムカつく｡原因が特定できない｡ありえん｡
+
+uses: mikefarah/yq@master
+
+として yq を使うと､引数に指定したファイルがないって言われる
+
+sudo curl  ~
+として yq を install しようとすると sudo command is not found と言われる
+curl も notfound だったし｡どうすればいいのか皆目見当がつかない
